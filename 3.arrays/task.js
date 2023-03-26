784bbf54f7c4af82648f5550c6d719be04849124
Denis Khusainov <denis.khusainov.2017@mail.ru>
2023-03-26 19:34:37 +0300

    Error correcion

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,6 @@
-function compareArrays(arr1, arr2) {
-  
-}
+let compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((i, n) => i === arr2[n])
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+function getUsersNamesInAgeRange(users, genders){
+    return users.filter( costumer => costumer.gender === genders).map(ageGenders =>ageGenders.age).reduce((a, b, ind, arr) => a + b /arr.length, 0)
+ 
 }
